b5ff40074aa2f65ef2e429193f74beaa11bdfa9d
Hurlika Muhammattursun <hurlikamuhammattursun@Hurlikas-MacBook-Pro.local>
2020-04-28 15:43:02 -0400

    add HappyBirthday.java

---
+++ src/day_1/addCommand.java
@@ -0,0 +1,10 @@
+package day_1;
+
+import java.util.jar.JarOutputStream;
+
+public class addCommand {
+    public static void main(String[] args) {
+
+        System.out.println("hello hello");
+    }
+}
